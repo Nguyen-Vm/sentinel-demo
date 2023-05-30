e060a48d1236491e1f36178a50a6e76e27e2bdd9
ryan <ruanweimin@enmonster.com>
2023-05-30 13:52:26 +0800

    Sentinel注解支持

--- src/main/java/com/example/sentinel/service/ParamFlowService.java
+++ src/main/java/com/example/sentinel/service/ParamFlowService.java
@@ -1,10 +1,5 @@
 package com.example.sentinel.service;
 
-import com.alibaba.csp.sentinel.EntryType;
-import com.alibaba.csp.sentinel.annotation.SentinelResource;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-
 /**
  * @author 阮威敏
  * @date 2023/5/30
--- src/main/java/com/example/sentinel/service/impl/ParamFlowServiceImpl.java
+++ src/main/java/com/example/sentinel/service/impl/ParamFlowServiceImpl.java
@@ -1,9 +1,6 @@
 package com.example.sentinel.service.impl;
 
-import com.alibaba.csp.sentinel.Entry;
-import com.alibaba.csp.sentinel.EntryType;
-import com.alibaba.csp.sentinel.SphU;
-import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.example.sentinel.service.ParamFlowService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -16,18 +13,9 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class ParamFlowServiceImpl implements ParamFlowService {
 
+    @SentinelResource("paramFlow")
     @Override
     public void paramFlow(String paramA, String paramB) {
-        Entry entry = null;
-        try {
-            entry = SphU.entry("paramFlow", EntryType.IN, 1, paramA, paramB);
-        } catch (BlockException e) {
-            throw new RuntimeException(e);
-        } finally {
-            if (entry != null) {
-                entry.exit(1, paramA, paramB);
-            }
-        }
         log.info("paramA: {}, paramB: {}", paramA, paramB);
     }
 }
